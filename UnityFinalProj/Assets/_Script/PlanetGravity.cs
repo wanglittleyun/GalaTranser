28b2662791b818ad14fab070fe9c62fd5b9377c2
Rolo <rolo_lamperouge@foxmail.com>
2015-12-16 08:11:33 +0800

    update basic test scene
    update basic test scene.
    we need to rescale the ship. lower the speed, and to show the feel of
    speed by using particle effect

---
+++ UnityFinalProj/Assets/_Script/PlanetGravity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+	public GameObject player;
+	public float Power_distance = 2.0f; 
+	public float forceMagnitude = 100000.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 force = (transform.position - player.transform.position) * forceMagnitude
+			/Mathf.Pow(Vector3.Magnitude(transform.position- player.transform.position),Power_distance);
+		player.GetComponent<Rigidbody> ().AddForce (force);
+	}
+}
