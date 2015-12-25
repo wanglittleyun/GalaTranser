adf8b4440292d7102dd9c11fa14e6265bf208cde
Rolo <rolo_lamperouge@foxmail.com>
2015-12-25 14:03:39 +0800

    test scene

--- UnityFinalProj/Assets/_Script/PlanetGravity.cs
+++ UnityFinalProj/Assets/_Script/PlanetGravity.cs
@@ -5,16 +5,36 @@ public class PlanetGravity : MonoBehaviour {
 	public GameObject player;
 	public float Power_distance = 2.0f; 
 	public float forceMagnitude = 100000.0f;
+	warning warnship;
+	bool inSight;
 	// Use this for initialization
 	void Start () {
-	
+		inSight = false;
+		warnship = player.GetComponent<warning> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (inSight) {
+			Vector3 force = (transform.position - player.transform.position) * forceMagnitude
+				/ Mathf.Pow (Vector3.Magnitude (transform.position - player.transform.position), Power_distance);
+			player.GetComponent<Rigidbody> ().AddForce (force);
+			warnship.gravity_warn(transform.position, 1);
+		}
+	}
 
-		Vector3 force = (transform.position - player.transform.position) * forceMagnitude
-			/Mathf.Pow(Vector3.Magnitude(transform.position- player.transform.position),Power_distance);
-		player.GetComponent<Rigidbody> ().AddForce (force);
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == Tags.Player) {
+			Debug.Log ("OnTrigger");
+			inSight = true;
+		}
+	}
+	void OnTriggerExit(Collider other){
+
+		if (other.tag == Tags.Player) {
+			Debug.Log ("Exit trigger");
+			inSight = false;
+		}
 	}
 }
---
+++ UnityFinalProj/Assets/_Script/warning.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class warning : MonoBehaviour {
+	public Camera camera;
+
+	public Texture2D cross;//when we are catched by a planet, we will render this texture in it's place
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI(){
+
+	}
+
+	public void gravity_warn(Vector3 planet_world_coor, int flag){
+		Vector3 window_vec = camera.WorldToScreenPoint (planet_world_coor);
+		Rect rect = new Rect (new Vector2 (window_vec.x, window_vec.y), new Vector2(20.0f,20.0f));
+		GUI.DrawTexture (rect,cross);
+	}
+}
