1b10c9118691177b2c74e04757457d92d3599400
Rolo <rolo_lamperouge@foxmail.com>
2015-12-27 18:36:32 +0800

    add comment to some script
    add some comment.
    //TO-DO
    1\set layer setting to the meteors
    2\add more particle

--- UnityFinalProj/Assets/_Script/PlanetGravity.cs
+++ UnityFinalProj/Assets/_Script/PlanetGravity.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/* PlanetGravity
+ * ===================
+ * this script is bind to a planet
+ * when player is getting too near to the planet, it'll receive a drag force form the player to the planet
+ * this force will geting stronger when the player is nearer to the planet
+ */
 public class PlanetGravity : MonoBehaviour {
+	// player
 	public GameObject player;
+	// the power of distance
 	public float Power_distance = 2.0f; 
+	// the force when distance is 1
 	public float forceMagnitude = 100000.0f;
+	// a constant flag to let the alarm and warning system know which
+	// planet if draging the space ship
 	public int planet_flag;
+	// the warning script binded to the player
+	// used to inform player that their space ship is caught by a planet
 	warning warnship;
+	// to see if the planet is going to drag the player
 	bool inSight;
 	// Use this for initialization
 	void Start () {
@@ -16,6 +30,7 @@ public class PlanetGravity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// if the player is too near to the planet, the planet will add a force to the space ship
 		if (inSight) {
 			Vector3 force = (transform.position - player.transform.position) * forceMagnitude
 				/ Mathf.Pow (Vector3.Magnitude (transform.position - player.transform.position), Power_distance);
@@ -23,7 +38,7 @@ public class PlanetGravity : MonoBehaviour {
 
 		}
 	}
-
+	//change the state when player enter the range
 	void OnTriggerEnter(Collider other){
 
 		if (other.tag == Tags.Player) {
@@ -32,6 +47,7 @@ public class PlanetGravity : MonoBehaviour {
 			inSight = true;
 		}
 	}
+	// change the state when palyer leave the range
 	void OnTriggerExit(Collider other){
 
 		if (other.tag == Tags.Player) {
