9a6af40df61560c24c31f2eb76956d03e44f9e32
Rolo <rolo_lamperouge@foxmail.com>
2016-01-18 05:03:02 +0800

    control test
    not very well

---
+++ UnityFinalProj/Assets/_Script/testscripts/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+/*utilities that may use in other scripts*/
+public class Utilities  {
+	public static float MilePerHour_to_MeterPerSecond(float v){
+		return (float)(v * 0.44704);
+	}
+
+	public static float MeterPerSecond_to_MilePerHour(float v){
+		return (float)(v * 2.23693629);
+	}
+
+	/* Compare the sign of two float number
+	 * Mathf.Sign returns 1 whereas the in put is 0 or larger than 0
+	 * returns -1 when the input in smaller than 0*/
+	public static bool SameSigh(float first, float second){
+		if (Mathf.Sign (first) == Mathf.Sign (second))
+			return true;
+		else
+			return false;
+	}
+
+	/* Used to normalize drag force add to the car*/
+	public static float EvaluateNormPower(float normPower){
+		if (normPower < 1)
+			return 10 - normPower * 9;
+		else
+			return 1.9f - normPower * 0.9f;
+	}
+
+
+}
---
+++ UnityFinalProj/Assets/_Script/testscripts/control.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+/* New test movement control script*/
+public class control : MonoBehaviour {
+	/*mass center*/
+	public Transform centerOfMass;
+	//maximum speed, miles per hour
+	public float topSpeed = 160.0f;
+	//throttle
+	public float throttle = 0.0f;
+	//steer(truning left or right)
+	private float steer = 0.0f;
+	//rigidbody component of the player
+	private Rigidbody rb;
+	/* turning coefficient of the turning
+	 * when the speed approach topSpeed, current turning coefficient will approach minTurn
+	 * when the speed approach 0, current turining coefficient will approach maxTurn*/
+	const float minTurn = 10;
+	const float maxTurn = 15;
+
+	// Use this for initialization
+	void Start () {
+		//initialization
+		rb = GetComponent<Rigidbody> ();
+		//rb.centerOfMass = centerOfMass.localPosition;
+		// top speed is transferred to meters per second here
+		topSpeed = Utilities.MilePerHour_to_MeterPerSecond (topSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		throttle = Input.GetAxis ("Vertical");
+		steer = Input.GetAxis ("Horizontal");
+	}
+	// fixedupdate is called at the end of the pipeline
+	void FixedUpdate(){
+		Vector3 rv = transform.InverseTransformDirection (rb.velocity);
+		ApplySteering (rv);
+		ApplyThrottle ();
+	}
+	//use current speed to calculate current turning coefficient
+	float SpeedToTurn(float speed){
+		//threshold of minimum turning coefficient
+		float threshold = topSpeed / 2;
+		//if current speed is smaller than threshold, reutrn minTurn as current turning coefficient
+		if (speed > threshold)
+			return minTurn;
+		//calculate current turning coeffcient when
+		float speedIndex = 1 - (speed / threshold);
+		return minTurn + speedIndex * (maxTurn - minTurn);
+	}
+	void ApplySteering(Vector3 rv){
+		//turning radius
+		float turnRadius = (float)(3.0 / Mathf.Sin ((90 - (steer * 30)) * Mathf.Deg2Rad));
+		float currentTurningCoefficient = SpeedToTurn (rb.velocity.magnitude);
+		//turning speed
+		float turnSpeed = Mathf.Clamp (rv.z / turnRadius, -currentTurningCoefficient / 10, currentTurningCoefficient / 10);
+		//when turning right, steer > 0, otherwise steer < 0
+		transform.RotateAround (transform.position + transform.right * turnRadius * steer, transform.up, turnSpeed * Mathf.Rad2Deg * Time.deltaTime * steer);
+	}
+	//add and slow down the speed
+	void ApplyThrottle(){
+		rb.AddForce (transform.forward *20 * throttle * Time.deltaTime * rb.mass * 100);
+	}
+}
